3d9922f1f27000ece7eeba38edcfecc42e9d6942
joeyklee <jsphknglee@gmail.com>
2019-12-09 19:15:00 -0500

    testing options

--- db/db.js
+++ db/db.js
@@ -2,4 +2,5 @@ const mongoose = require('mongoose');
 const config = require('../config');
 const MONGODB_URI = config.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI, {useNewUrlParser:true, useCreateIndex: true});
+mongoose.connect(MONGODB_URI, {useNewUrlParser:true, 
+  useCreateIndex: true, useUnifiedTopology: true});
--- index.js
+++ index.js
@@ -37,10 +37,10 @@ const corsOptions = {
     credentials:true
 }
 
-
 // our middleware
 app.use(cookieParser())
-app.use('*', cors(corsOptions));
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(publicPath));
